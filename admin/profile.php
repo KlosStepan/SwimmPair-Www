bf86d44473daf4a1c6dd83bd71cd4ecf633c943d
Stepan Klos <stepanklos@gmail.com>
2023-01-08 00:57:15 +0100

    Profile menu refactor /admin

--- admin/profile.php
+++ admin/profile.php
@@ -4,15 +4,16 @@ require __DIR__ . '/../start.php';
 
 session_start();
 
-
 // Check if user is logged in using the session variable
-if ($_SESSION['logged_in'] != 1) {
+if ($_SESSION['logged_in'] != 1)
+{
     $_SESSION['message'] = "You must log in before viewing your profile page!";
     header("location: error.php");
-} else {
+}
+else
+{
     //Add unverified and unconfirmed FAIL or load administration
 
-    // Makes it easier to read
     $id = $_SESSION['id'];
     $first_name = $_SESSION['first_name'];
     $last_name = $_SESSION['last_name'];
@@ -21,20 +22,19 @@ if ($_SESSION['logged_in'] != 1) {
     $approved_flag = $_SESSION['approved_flag'];
     $rights = $_SESSION['rights'];
 	$affiliation_club_id = $_SESSION['affiliation_club_id'];
-    //$klubaffil = $_SESSION['klubaffil'];
 
     $adminBasedOnRights = "";
-    if ($rights == UserRights::SuperUser) {
+    if ($rights == UserRights::SuperUser)
+    {
         $adminBasedOnRights = "tmplcond2_superuser.php";
-        //include("tmplcond2_superuser.php");
     }
-    if ($rights == UserRights::VedouciKlubu) {
+    if ($rights == UserRights::VedouciKlubu)
+    {
         $adminBasedOnRights = "tmplcond1_vedouciklubu.php";
-        //include("tmplcond1_vedouciklubu.php");
     }
-    if ($rights == UserRights::Rozhodci) {
+    if ($rights == UserRights::Rozhodci)
+    {
         $adminBasedOnRights = "tmplcond0_smrtelnici.php";
-        //include("tmplcond0_smrtelnici.php");
     }
 
 }
@@ -46,14 +46,10 @@ if ($_SESSION['logged_in'] != 1) {
     <title>Welcome <?= $first_name . ' ' . $last_name ?></title>
     <?php include 'css/css.html'; ?>
     <?php include 'js/js.html'; ?>
-
-
 </head>
-
 <body>
-
 <?php
-$url = $_SERVER['HTTP_HOST'];
+    $url = $_SERVER['HTTP_HOST'];
 ?>
 <div class="form">
     <div id="hcontainer">
@@ -65,70 +61,53 @@ $url = $_SERVER['HTTP_HOST'];
         </a>
     </div>
     </div>
-
-    <!--<p>-->
     <?php
-
-    // Display message about account verification link only once
-    if (isset($_SESSION['message'])) {
-        echo $_SESSION['message'];
-
-        // Don't annoy the user with more messages upon page refresh
-        unset($_SESSION['message']);
-    }
-
+        // Display message about account verification link only once
+        if (isset($_SESSION['message']))
+        {
+            echo $_SESSION['message'];
+            // Don't annoy the user with more messages upon page refresh
+            unset($_SESSION['message']);
+        }
     ?>
-    <!--</p>-->
-
     <?php
-
-    // Keep reminding the user this account is not active, until they activate
-    if (!$active_flag) {
-        echo
-        '<div class="info">
-              Account is unverified, please confirm your email by clicking
-              on the email link!
-              </div>';
-    }
-
+        // Keep reminding the user this account is not active, until they activate
+        if (!$active_flag)
+        {
+            echo
+            '<div class="info">
+                  Account is unverified, please confirm your email by clicking
+                  on the email link!
+                  </div>';
+        }
     ?>
-
     <h2><?php echo $first_name . ' ' . $last_name; ?></h2>
     <hr style="height:1px;border:none;color:#333;background-color:white;">
-    <p></p>
-    <!--casing here-->
-	<?php //include($adminBasedOnRights);
-	if ($rights == UserRights::SuperUser) {
-		//$adminBasedOnRights = "tmplcond2_superuser.php";
-        include("tmplcond2_superuser.php");
-        //echo "SU";
-        include("tmplcond1_vedouciklubu.php");
-		//echo "Vedouci";
-		include("tmplcond0_smrtelnici.php");
-		//echo "Rozhodci";
-	}
-	if ($rights == UserRights::VedouciKlubu) {
-		//$adminBasedOnRights = "tmplcond1_vedouciklubu.php";
-		include("tmplcond1_vedouciklubu.php");
-		//echo "Vedouci";
-		include("tmplcond0_smrtelnici.php");
-		//echo "Rozhodci";
-	}
-	if ($rights == UserRights::Rozhodci) {
-		//$adminBasedOnRights = "tmplcond0_smrtelnici.php";
-		include("tmplcond0_smrtelnici.php");
-		//echo "Rozhodci";
-	}
+    <p>&nbsp;</p>
+	<?php
+	    if ($rights == UserRights::SuperUser)
+        {
+            include("tmplcond2_superuser.php");
+            include("tmplcond1_vedouciklubu.php");
+    		include("tmplcond0_smrtelnici.php");
+    	}
+    	if ($rights == UserRights::VedouciKlubu)
+        {
+	    	include("tmplcond1_vedouciklubu.php");
+		    include("tmplcond0_smrtelnici.php");
+	    }
+	    if ($rights == UserRights::Rozhodci)
+        {
+		    include("tmplcond0_smrtelnici.php");
+	    }
 	?>
-
     <a href="logout.php">
         <button class="button button-block" name="logout"/>
-        Odhlásit se</button></a>
-
+            Odhlásit se
+        </button>
+    </a>
 </div>
-
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src="js/index.js"></script>
-
 </body>
 </html>
--- admin/tmplcond2_superuser.php
+++ admin/tmplcond2_superuser.php
@@ -1,4 +1,3 @@
-<!--<p align="center">Vy jste určitě <span class="red">správce</span>!</p>-->
 <h3>ADMINISTRATIVA</h3>
 <ul id="nabidkavadminu">
     <li class="left-split" onclick="location.href = 'pridat_aktualitu.php';"><div>PŘIDAT AKTUALITU</div></li>
@@ -6,14 +5,11 @@
     <li onclick="location.href = 'nove_registrovani.php';"><div>SCHVÁLIT NOVÉ LIDI</div></li>
     <li onclick="location.href = 'rozhodci_zavody.php';"><div>PŘIŘAZOVÁNÍ ROZHODČÍCH NA POZICE</div></li>
     <li class="left-split" onclick="location.href = 'zaregistrovat_uzivatele.php';"><div>ZAREGISTROVAT ČLOVĚKA</div></li>
-    <!-- ADDING THIS SHIT, NOT IN THE MOBILE APP -->
     <li class="right-split" onclick="location.href = 'editovat_profily.php';"><div>EDITOVAT ČLOVĚKA</div></li>
     <li class="left-split" onclick="location.href = 'novy_klub.php';" ><div>PŘIDAT KLUB</div></li>
     <li class="right-split" onclick="location.href = 'sprava_klubu.php';"><div>EDITOVAT KLUB</div></li>
     <li class="left-split" onclick="location.href = 'novy_kraj.php';" ><div>PŘIDAT KRAJ</div></li>
     <li class="right-split" onclick="location.href = 'sprava_kraju.php';"><div>EDITOVAT KRAJ</div></li>
     <li onclick="location.href = 'konfigurace_statistik.php';"><div>KONFIGURACE STATISTIK</div></li>
-    <!--<li onclick="location.href = 'nova_pravidla.php';"><div>NAHRÁT NOVÁ PRAVIDLA</div></li>-->
     <li onclick="location.href = 'editovat_stranku.php?id=1';"><div>EDITOVAT KONTAKTY</div></li>
-    <!--<li><a href="other.php">OTHER MAYBE...</a></li>-->
 </ul>
