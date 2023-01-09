0505080a66999dcbb13c799b81445d8901cea840
Stepan Klos <stepanklos@gmail.com>
2023-01-09 17:28:23 +0100

    logic into Ajax call smaller manager functions calls

--- admin/XMLHttpRequest/update_config_stats_positions.php
+++ admin/XMLHttpRequest/update_config_stats_positions.php
@@ -11,12 +11,45 @@ if (json_last_error()!==JSON_ERROR_NONE)
 	echo "RuntimeException(), no commit";
 	throw new RuntimeException("Decode json error - failed");
 }
-
-if($positionsManager->UpdateStatsPositions($json_processed))
+//DEPRECATE SOON
+/*if($positionsManager->UpdateStatsPositions($json_processed))
 {
 	echo "succ<br/>\r\n";
 }
 else
 {
 	echo "err<br/>\r\n";
+}*/
+$mysqli->begin_transaction();
+try
+{
+	if(!($positionsManager->DeleteOldStatsPositions()))
+	{
+		echo "Delete old stats-positions. - failed";
+		throw new RuntimeException("Delete old stats-positions - failed");
+	}
+	foreach ($json_processed as $record)
+	{
+		if (!isset($record["idpoz"]))
+		{
+			throw  new RuntimeException();
+		}
+		elseif (!ctype_digit($record["idpoz"]))
+		{
+			throw new RuntimeException();
+		}
+		if(!($positionsManager->InsertNewStatPosition($record["idpoz"])))
+		{
+			echo "New stat insert {$record["idpoz"]}- failed";
+			throw new RuntimeException("New stat insert {$record["idpoz"]} - failed");
+		}
+	}
+	$mysqli->commit();
+	echo "succ<br/>\r\n";
+}
+catch (RuntimeException $e)
+{
+	$mysqli->rollback();
+	echo "err<br/>\r\n";
+	echo $e->getMessage();
 }
