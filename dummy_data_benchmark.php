4654137ef6ab213778d4c769b2030ff47f1fb8f6
Stepan Klos <stepanklos@gmail.com>
2023-01-14 00:12:28 +0100

    Dummy data and benchmark script OK

--- dummy_data_benchmark.php
+++ dummy_data_benchmark.php
@@ -13,17 +13,22 @@ $content = array("Pellentesque lacinia mollis pharetra. Praesent sit amet ligula
 //$y = $usersManager->FindAllActiveUsersOrderByLastNameAsc();
 //print_r($y)
 
-//Start script
-$start = microtime(true);
+//https://www.proose.com/tools/php-fiddle
+
 //Retrieve existing info
 $ranks = $usersManager->FindAllRefereeRanks();
 $clubs = $clubsManager->FindAllClubs();
-//create 98 users - random affil to 1-15
-//https://www.proose.com/tools/php-fiddle
-echo ("Users #3-#100<br/>\r\n");
+
+echo ("SWIMMPAIR DUMMY DATA & BENCHMARK<br/>\r\n");
+//Start script
+$start = microtime(true);
+$rt_so_far = 0;
+
+//1. Create 98 Users - random affil to 1-15
+echo ("1. Register Users #3-#100<br/>\r\n");
 for($i=3;$i<=100;++$i)
 {
-    echo ($i."\r\n");
+    //echo ($i."\r\n");
     $first_name;
     $last_name;
     if (rand(0, 1))
@@ -38,53 +43,100 @@ for($i=3;$i<=100;++$i)
     }
     $email_idx = array_rand($emails, 1);
     $email = strtolower($first_name).strtolower($last_name)."@".$emails[$email_idx];
-    //$password = "...";
+    //$password = 12345;
     $rights_idx = array_rand($rights, 1);
     $rrid_idx = array_rand($ranks, 1);
     $club_idx = array_rand($clubs, 1);
-    echo "\$usersManager->CreateUser(".$first_name.",".$last_name.",".$email.",12345,".$rights[$rights_idx].",".$ranks[$rrid_idx]->id.",".$clubs[$club_idx]->id.");<br/>\r\n";
+    //echo "\$usersManager->RegisterUser(".$first_name.",".$last_name.",".$email.",12345,".$rights[$rights_idx].",".$ranks[$rrid_idx]->id.",".$clubs[$club_idx]->id.");<br/>\r\n";
+    $usersManager->RegisterUser($first_name, $last_name, $email, 12345, $rights[$rights_idx], $ranks[$rrid_idx]->id, $clubs[$club_idx]->id);
 }
-echo ("Clubs #1-#12<br/>\r\n");
+$stop1 = microtime(true) - $start;
+$rt_so_far = $stop1;
+echo "Stop 1: $stop1 sec.<br/>\r\n";
+
+//2. Create 12 Cups
+echo ("2. Insert Cups #1-#12<br/>\r\n");
 for($j=1;$j<=12;++$j)
 {
-    echo ($j."\r\n");
+    //echo ($j."\r\n");
     $cup_name_idx = array_rand($cups_names, 1);
     $club_idx = array_rand($clubs, 1);
     $content_idx = array_rand($content, 1);
-    echo "\$clubsManager->InsertNewCup(".$cups_names[$cup_name_idx]." ".rand(1, 8).".,2023-".str_pad($j, 2, '0', STR_PAD_LEFT)."-26, 2023-".str_pad($j, 2, '0', STR_PAD_LEFT)."-28,".$clubs[$club_idx]->id.",".$content[$content_idx].");<br/>\r\n";
+    //echo "\$cupsManager->InsertNewCup(".$cups_names[$cup_name_idx]." ".rand(1, 8).".,2023-".str_pad($j, 2, '0', STR_PAD_LEFT)."-26, 2023-".str_pad($j, 2, '0', STR_PAD_LEFT)."-28,".$clubs[$club_idx]->id.",".$content[$content_idx].");<br/>\r\n";
+    $cupsManager->InsertNewCup($cups_names[$cup_name_idx]." ".rand(1, 8), "2023-".str_pad($j, 2, '0', STR_PAD_LEFT)."-26", "2023-".str_pad($j, 2, '0', STR_PAD_LEFT)."-28", $clubs[$club_idx]->id, $content[$content_idx]);
 }
-echo ("Availability XX<br/>\r\n");
-for($k=1;$k<=5;++$k)
-{
-    echo ("avail $k<br/>\r\n");
-}
-echo ("Pairing YY<br/>\r\n");
-for($l=1;$l<=5;++$l)
-{
-    echo ("pairing $l<br/>\r\n");
-}
-//10 random queries Club(1q)/User(2qs) stats (uid, year)
-for($m=1;$m<=10;++$m)
-{
-    echo ("Q #$l<br/>\r\n");
-}
-//availability for each cup 300
-//foreach Cups as cup
-//for 1 to 30 random user
-//available(cup, person, 1)
+$stop2 = microtime(true) - $start - $rt_so_far;
+$rt_so_far = $stop2;
+echo "Stop 2: $stop2 sec.<br/>\r\n";
 
-//foreach Cups as cup
-//getAvailableUsers(cup)
-//foreach availableUser as avlusr
-//pairing(cip, avlusr-id, positions[position-idx])
+//3. Fetch new Users and Cups (&positions)
+echo ("3. Fetch Users and Cups (&Positions)<br/>\r\n");
+$users = $usersManager->FindAllActiveUsersOrderByLastNameAsc();
+$cups = $cupsManager->FindAllUpcomingCupsEarliestFirst();
+$positions = $positionsManager->FindAllPositions();
 
+$stop3 = microtime(true) - $start - $rt_so_far;
+$rt_so_far = $stop3;
+echo "Stop 3: $stop3 sec.<br/>\r\n";
+
+//4. Create Availabilities
+echo ("4. Insert Availability (20 per Cup)<br/>\r\n");
+for($k=0; $k<count($cups); ++$k)
+{
+    $kk_avails=20;
+    $user_idx = array_rand($users, $kk_avails);
+    for($kk=0; $kk<$kk_avails; ++$kk)
+    {
+        $cupsManager->InsertNewAvailability($cups[$k]->id, $users[$user_idx[$kk]]->id, 1);
+    }
+}
+$stop4 = microtime(true) - $start - $rt_so_far;
+$rt_so_far = $stop4;
+echo "Stop 4: $stop4 sec.<br/>\r\n";
+
+//5. Create Pairings
+echo ("5. Insert Pairing (availabilities 1 random pos. for each)<br/>\r\n");
+for($l=0; $l<count($cups); ++$l)
+{
+    $avails = $usersManager->FindAllRegisteredUsersForTheCup($l+1);
+    for($ll=0; $ll<count($avails); ++$ll)
+    {
+        $position_idx = array_rand($positions, 1);
+        $cupsManager->InsertNewPairing(($l+1), $positions[$position_idx]->id, $avails[$ll]->id);
+    }
+}
+$stop5 = microtime(true) - $start - $rt_so_far;
+$rt_so_far = $stop5;
+echo "Stop 5: $stop5 sec.<br/>\r\n";
+
+//6. Queries for Clubs / Users in random order
+echo("6. Call stats queries (20 either Clubs/Users stat queryings)<br/>\r\n");
+for($m=1;$m<=20;++$m)
+{
+    if (rand(0, 1))
+    {
+        //echo "-user query $m-<br/>\r\n";
+        $year = $cupsManager->GetMaximumCupYear();
+        $user_idx = array_rand($users, 1);
+        $personCupsCount = $usersManager->CountCupsAttendanceOfUserGivenYear($users[$user_idx]->id, $year);
+        $stats = $usersManager->CountOverallStatisticsOfUserGivenYear($users[$user_idx]->id, $year);
+        //echo $personCupsCount."<br/>\r\n";
+        //print_r($stats);
+    }
+    else
+    {
+        //echo "-club query $m-<br/>\r\n";
+        $year = $cupsManager->GetMaximumCupYear();
+        $club_idx = array_rand($clubs, 1);
+        $stats = $usersManager->CountClubSeasonalStatistics($clubs[$club_idx]->id, $year);
+        //print_r($stats);
+    }
+}
+$stop6 = microtime(true) - $start - $rt_so_far;
+$rt_so_far = $stop6;
+echo "Stop 6: $stop6 sec.<br/>\r\n";
+//
 $total = microtime(true) - $start;
-echo $total;
-echo "<br/>\r\n";
+echo "TOTAL RUNTIME: $total sec.<br/>\r\n";
 
-//$club_idx = array_rand($clubs, 1);
-//print_r($clubs[$club_idx]->id);
-//echo "<br/>\r\n";
-//print_r($clubs[$club_idx]);
-//echo "<br/>\r\n";
 ?>
