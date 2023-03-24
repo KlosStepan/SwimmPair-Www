efe707855055a5b91f01896c80ace46d46da0842
Stepan Klos <stepanklos@gmail.com>
2023-03-24 17:49:21 +0100

    PHPUnit config w/ sample test - working

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_asserts_1()
+    {
+        $this->assertTrue(true);
+        $this->assertEquals([1, 2, 3], [1, 2, 4]);
+
+        return true;
+    }
+}
