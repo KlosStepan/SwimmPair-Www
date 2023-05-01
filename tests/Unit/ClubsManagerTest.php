9a614e7bb97774beb8cd834e6b8d001b9a450da9
Stepan Klos <stepanklos@gmail.com>
2023-05-02 00:45:44 +0200

    ClubsManagerTest run pipeline  w/ new CRUD tests

--- tests/Unit/ClubsManagerTest.php
+++ tests/Unit/ClubsManagerTest.php
@@ -18,4 +18,93 @@ class ClubsManagerTest extends TestCase
         $this->assertEquals([1, 2, 3], [1, 2, 3]);
         return true;
     }
+    public function testFindAllClubsReturnsArray()
+    {
+        $clubs = $this->cupsManager->FindAllClubs();
+        $this->assertIsArray($clubs);
+    }
+
+    public function testFindAllClubsReturnsEmptyArrayWhenNoClubs()
+    {
+        $clubs = $this->cupsManager->FindAllClubs();
+        $this->assertEmpty($clubs);
+    }
+
+    public function testGetClubAffiliationToRegionReturnsValidRegionId()
+    {
+        $clubId = 1;
+        $regionId = $this->cupsManager->GetClubAffiliationToRegion($clubId);
+        $this->assertNotNull($regionId);
+    }
+
+    public function testGetClubAffiliationToRegionReturnsNullWithInvalidClubId()
+    {
+        $clubId = -1;
+        $regionId = $this->cupsManager->GetClubAffiliationToRegion($clubId);
+        $this->assertNull($regionId);
+    }
+
+    public function testGetClubByIDReturnsValidClubObject()
+    {
+        $clubId = 1;
+        $club = $this->cupsManager->GetClubByID($clubId);
+        $this->assertInstanceOf(Club::class, $club);
+    }
+
+    public function testGetClubByIDReturnsNullWithInvalidClubId()
+    {
+        $clubId = -1;
+        $club = $this->cupsManager->GetClubByID($clubId);
+        $this->assertNull($club);
+    }
+
+    public function testInsertNewClubSuccessfullyInsertsClub()
+    {
+        $name = 'Test Club';
+        $abbreviation = 'TC';
+        $clubId = 100;
+        $img = 'test_image.png';
+        $regionId = 1;
+
+        $success = $this->cupsManager->InsertNewClub($name, $abbreviation, $clubId, $img, $regionId);
+        $this->assertTrue($success);
+    }
+
+    public function testInsertNewClubReturnsFalseWithInvalidInput()
+    {
+        $name = 'Invalid Club';
+        $abbreviation = 'IC';
+        $clubId = 1;
+        $img = 'invalid_image.png';
+        $regionId = -1;
+
+        $success = $this->cupsManager->InsertNewClub($name, $abbreviation, $clubId, $img, $regionId);
+        $this->assertFalse($success);
+    }
+
+    public function testUpdateClubSuccessfullyUpdatesClub()
+    {
+        $id = 1;
+        $name = 'Updated Club';
+        $abbreviation = 'UC';
+        $code = '123';
+        $img = 'updated_image.png';
+        $regionId = 2;
+
+        $success = $this->cupsManager->UpdateClub($id, $name, $abbreviation, $code, $img, $regionId);
+        $this->assertTrue($success);
+    }
+
+    public function testUpdateClubReturnsFalseWithInvalidInput()
+    {
+        $id = -1;
+        $name = 'Invalid Club';
+        $abbreviation = 'IC';
+        $code = '';
+        $img = 'invalid_image.png';
+        $regionId = -1;
+
+        $success = $this->cupsManager->UpdateClub($id, $name, $abbreviation, $code, $img, $regionId);
+        $this->assertFalse($success);
+    }
 }
