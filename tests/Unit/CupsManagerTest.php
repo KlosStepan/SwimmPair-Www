eddaca6c8c39ce277c939d2424c4eb1d426b4cb4
Stepan Klos <stepanklos@gmail.com>
2023-05-02 03:04:12 +0200

    CupsManagerTest pipeline run w/ first test added

--- tests/Unit/CupsManagerTest.php
+++ tests/Unit/CupsManagerTest.php
@@ -7,6 +7,12 @@ use PHPUnit\Framework\TestCase;
 require_once((dirname(dirname(__DIR__))) . '/start.php');
 class CupsManagerTest extends TestCase
 {
+    protected $cupsManager;
+    protected function setUp()
+    {
+        global $cupsManager;
+        $this->cupsManager = $cupsManager;
+    }
     /**
      * A basic test example.
      */
@@ -46,6 +52,7 @@ class CupsManagerTest extends TestCase
     $this->assertIsString($hash2);
     $this->assertEquals($hash1, $hash2);
     }*/
+
     public function test_GetMaximumCupYear()
     {
         global $cupsManager;
@@ -60,6 +67,23 @@ class CupsManagerTest extends TestCase
         global $cupsManager;
         $available = $cupsManager->IsUserAvailableForTheCup($userID, $cupID);
         $this->assertIsBool($available);
+    }
+    public function testInsertCupAndCheckIfUserIsAvailable()
+    {
+        // Insert a new cup
+        $name = "Test Cup";
+        $date_begin = "2023-06-01";
+        $date_end = "2023-06-05";
+        $club = "Test Club";
+        $content = "Test Content";
+        $cupID = $this->cupsManager->InsertNewCup($name, $date_begin, $date_end, $club, $content);
+
+        // Check if a user is available for the cup
+        $userID = 1;
+        $isAvailable = $this->cupsManager->IsUserAvailableForTheCup($userID, $cupID);
+
+        // Assert that the user is not available by default
+        $this->assertFalse($isAvailable);
     }
 
 }
